d0ca8b336dce7fb6619fe46929d8f399cfe3d5ba
dotfn <dotfn@users.noreply.github.com>
2026-06-21 06:04:17 -0300

    Multi-arch cask with on_arm/on_intel

--- Casks/lumus-control.rb
+++ Casks/lumus-control.rb
@@ -1,9 +1,17 @@
 cask "lumus-control" do
   version "0.2.0"
-  sha256 :no_check
 
-  url "https://github.com/dotfn/lumus-control/releases/download/v#{version}/lumus-control_#{version}_aarch64.dmg",
-      verified: "github.com/dotfn/lumus-control/"
+  on_arm do
+    url "https://github.com/dotfn/lumus-control/releases/download/v#{version}/lumus-control_#{version}_aarch64.dmg",
+        verified: "github.com/dotfn/lumus-control/"
+    sha256 :no_check
+  end
+  on_intel do
+    url "https://github.com/dotfn/lumus-control/releases/download/v#{version}/lumus-control_#{version}_x86_64.dmg",
+        verified: "github.com/dotfn/lumus-control/"
+    sha256 :no_check
+  end
+
   name "Lumus Control"
   desc "Desktop control application for Lumus devices"
   homepage "https://github.com/dotfn/lumus-control"
